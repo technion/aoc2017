c1d07da7d79e12716a9e9130d8326604b003db90
Technion <technion@lolware.net>
2017-12-05 09:25:00 +0000

    Five in ruby

---
+++ five.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+def solvefive(maze)
+  offset = 0
+  moves = 0
+
+  while(offset >= 0 && offset < maze.length) do
+    oldoffset = maze[offset]
+    maze[offset] = oldoffset >= 3 ? oldoffset - 1 : oldoffset + 1
+    offset = offset + oldoffset
+    moves = moves + 1
+  end
+  moves
+end
+
+
+example = [0, 3, 0, 1, -3]
+
+puts solvefive(example)
+
+maze = File.read("five.txt").split("\n").map(&:to_i)
+puts solvefive(maze)
+
